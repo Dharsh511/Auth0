2e9050733d18f3d60b152cd3109033b2d606c025
Elangovan <dharshini.elangovan@hidglobal.com>
2024-08-02 11:04:06 +0530

    initial commit

---
+++ webflux-login/src/main/java/com/auth0/example/SecurityConfig.java
@@ -0,0 +1,62 @@
+package com.auth0.example;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.web.server.authentication.logout.RedirectServerLogoutSuccessHandler;
+import org.springframework.security.web.server.authentication.logout.ServerLogoutSuccessHandler;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+/**
+ * Configures the application's security settings
+ */
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+
+    @Value("https://dev-wzekeqw0stfkmwzq.us.auth0.com/")
+    private String issuer;
+    @Value("zW30ocZKqRGDmJWiQq5FzVRjhvlTWAHX")
+    private String clientId;
+
+    @Bean
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+        return http.authorizeExchange(authorize -> {
+                    authorize.pathMatchers("/", "/images/**").permitAll();
+                    authorize.anyExchange().authenticated();
+                })
+            .oauth2Login(withDefaults())
+            .logout(logout ->
+                logout.logoutSuccessHandler(logoutSuccessHandler())
+            ).build();
+    }
+
+    /**
+     * Configures the logout handling to log users out of Auth0 after successful logout from the application.
+     * @return a {@linkplain ServerLogoutSuccessHandler} that will be called on successful logout.
+     */
+    @Bean
+    public ServerLogoutSuccessHandler logoutSuccessHandler() {
+        // Change this as needed to URI where users should be redirected to after logout
+        String returnTo = "http://localhost:3000/";
+
+        // Build the URL to log the user out of Auth0 and redirect them to the home page.
+        // URL will look like https://YOUR-DOMAIN/v2/logout?clientId=YOUR-CLIENT-ID&returnTo=http://localhost:3000
+        String logoutUrl = UriComponentsBuilder
+        .fromHttpUrl(issuer + "v2/logout?client_id=zW30ocZKqRGDmJWiQq5FzVRjhvlTWAHX&returnTo=http://localhost:3000")
+        .encode()
+        .toUriString();
+    
+
+        RedirectServerLogoutSuccessHandler handler = new RedirectServerLogoutSuccessHandler();
+        handler.setLogoutSuccessUrl(URI.create(logoutUrl));
+        return handler;
+    }
+}
